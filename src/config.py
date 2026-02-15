c939093d214fe2660c3068655dd6a107dc72d436
kev1n999 <sopratesta999@gmail.com>
2026-02-15 14:47:21 -0300

    fix return of database connect function

--- src/config.py
+++ src/config.py
@@ -6,3 +6,4 @@ load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
 DB = mongo_connect(MONGO_URI)
+TASKS_COLLECTION = DB.get_collection("tasks")
--- src/db/connect.py
+++ src/db/connect.py
@@ -1,11 +1,13 @@
-from pymongo import MongoClient
+from pymongo import MongoClient, database
 
-def mongo_connect(mongo_uri: str | None) -> MongoClient | None:
+def mongo_connect(mongo_uri: str | None) -> database.Database | None:
   try:
     if mongo_uri is None:
       raise Exception("Mongo URI is missing!!")
+
     mongo_client = MongoClient(mongo_uri)
+    database = mongo_client.get_database("todolist")
     print("Connect with MongoDB!")
-    return mongo_client
+    return database
   except Exception as err:
     print(f"an error ocurred to connect with pymongo!\n{err}")
